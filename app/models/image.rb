c55d03b3f3c205f6164bb4c78a851c8513474cba
Lauren <lauren.tewksbury@appfolio.com>
2018-10-26 15:50:21 -0700

    lt - code review feedback

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,6 +4,11 @@ class Image < ApplicationRecord
   validate :url_valid?,
            :extension_valid?
 
+  JPG = '.jpg'.freeze
+  JPEG = '.jpeg'.freeze
+  PNG = '.png'.freeze
+  SUPPORTED_IMAGE_EXTENSIONS = [JPG, JPEG, PNG].freeze
+
   private
 
   # checks that start of url is properly formed
@@ -17,13 +22,9 @@ class Image < ApplicationRecord
     errors.add(:url, 'invalid') unless url.is_a?(URI::HTTP) || url.is_a?(URI::HTTPS)
   end
 
-  JPG = '.jpg'.freeze
-  JPEG = '.jpeg'.freeze
-  PNG = '.png'.freeze
-
   # checks that user hasn't passed an invalid image extension
   def extension_valid?
-    return true if image_url.ends_with?(JPG) || image_url.ends_with?(JPEG) || image_url.ends_with?(PNG)
+    return true if SUPPORTED_IMAGE_EXTENSIONS.any? { |ext| image_url.ends_with?(ext) }
     errors.add(:extension, 'unsupported/invalid')
     false
   end
