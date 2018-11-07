42ca39fadb06dff541532a7a6a37eaaece1e58a1
Lauren <lauren.tewksbury@appfolio.com>
2018-11-07 12:05:10 -0800

    lt - configure sendmail

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # No spam during testing
+  ActionMailer::Base.delivery_method = :test
 end
