eb917278b591098feb320b6337a1827b22ccbd7f
Lauren <lauren.tewksbury@appfolio.com>
2018-11-14 16:04:54 -0800

    lt - very wip

--- db/migrate/20181107221927_add_tag_if_empty.rb
+++ db/migrate/20181107221927_add_tag_if_empty.rb
@@ -2,26 +2,76 @@ class AddTagIfEmpty < ActiveRecord::Migration[5.2]
 
   PLACEHOLDER_TAG_NAME = '#placeholder'.freeze
   def up
-
+=begin
     #Search for all untagged images
     @untagged = Array.new
     Image.all.each do |img|
       @untagged.push img if ActsAsTaggableOn::Tagging.find_by(taggable_id: img.id).nil?
     end
 
-    #Create a new placeholder tag (if it doesn't already exist)
-    new_tag = ActsAsTaggableOn::Tag.find_by(name: PLACEHOLDER_TAG_NAME)
-    if new_tag.nil?
-      new_tag = ActsAsTaggableOn::Tag.create!(name: PLACEHOLDER_TAG_NAME) unless @untagged.empty?
-    end
-
     #Associate each image with this placeholder
     @untagged.each do |img|
       new_tagging = ActsAsTaggableOn::Tagging.create!(tag_id: new_tag.id, taggable_type: "Image", taggable_id: img.id, context: "tags")
     end
+=end
+  #<<~SQL
+  #   SELECT count(name) FROM tags WHERE name='#placeholder';
+  #SQL
+
+
+#=begin
+  #create the placeholder tag
+  execute <<~SQL
+    INSERT INTO tags (name) VALUES ('#placeholder');
+  SQL
+
+  #get the id of the placeholder tag
+  placeholder_id = execute <<~SQL
+    SELECT tags.id FROM tags WHERE tags.name='#placeholder';
+  SQL
+
+  # associate image with tag in taggings table
+  execute <<~SQL
+    INSERT INTO taggings(tag_id, taggable_id, taggable_type, context, created_at) SELECT #{placeholder_id[0]['id']}
+      AS tag_id, images.id, 'Image', 'tags', '#{Time.now}' FROM images
+      LEFT JOIN taggings ON images.id = taggings.taggable_id
+      LEFT JOIN tags ON taggings.tag_id = tags.id
+      WHERE tags.id is null;
+  SQL
+
+  # how many images have the placeholder tag?
+  placeholder_count = execute <<~SQL
+    SELECT Count(tag_id) FROM taggings WHERE tag_id = #{placeholder_id[0]['id']};
+  SQL
+
+  # update the placeholder_count value in tags with that value
+  execute <<~SQL
+    UPDATE tags SET taggings_count ="#{placeholder_count[0]['Count(tag_id)']}" WHERE name="#placeholder";
+  SQL
+#=end
+=begin
+
+    INSERT INTO taggings(tag_id, taggable_id, taggable_type, context) SELECT
+      (SELECT tags.id FROM tags WHERE tags.name="#placeholder")
+      AS tag_id, images.id, 'Image', 'tags' FROM images
+      LEFT JOIN taggings ON images.id = taggings.taggable_id
+      LEFT JOIN tags ON taggings.tag_id = tags.id
+      WHERE tags.id is null;
+
+  execute <<~SQL
+    INSERT INTO taggings(tag_id, taggable_id, taggable_type, context, created_at) SELECT
+      (SELECT tags.id FROM tags WHERE tags.name="#placeholder")
+      AS tag_id, images.id, 'Image', 'tags', '2018-11-07 23:38:46.698387' FROM images
+      LEFT JOIN taggings ON images.id = taggings.taggable_id
+      LEFT JOIN tags ON tags.id = taggings.tag_id
+      WHERE tags.name is null;
+  SQL
+=end
+
 end
 
   def down
+=begin
     #Which tag am I deleting
     tag_to_delete = ActsAsTaggableOn::Tag.find_by(name: PLACEHOLDER_TAG_NAME)
 
@@ -33,5 +83,21 @@ end
       end
     end
   end
+=end
+#=begin
+  #get the id of the placeholder tag
+  placeholder_id = execute <<~SQL
+    SELECT tags.id FROM tags WHERE tags.name='#placeholder';
+  SQL
+
+  execute <<~SQL
+    DELETE from taggings WHERE taggings.tag_id=#{placeholder_id[0]['id']};
+  SQL
+
+  execute <<~SQL
+    DELETE from tags where name='#placeholder';
+  SQL
+#=end
+end
 
 end
